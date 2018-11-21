5af1552463ee64ae95fcc13e0af5adc69f990aa0
cbg119 <cbagdon119@gmail.com>
2018-11-20 21:04:06 -0600

    ft_strcaat.c Initial Commit

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cbagdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 00:31:42 by cbagdon           #+#    #+#             */
-/*   Updated: 2018/11/20 20:55:35 by christian        ###   ########.fr       */
+/*   Updated: 2018/11/20 21:03:46 by christian        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int     ft_strlen(char *str);
 char    *ft_strdup(char *str);
 char    *ft_strcpy(char *dest, const char *src);
 char    *ft_strncpy(char *Dest, const char *src, size_t n);
+char    *ft_strcat(char *dest, const char *src);
 
 #endif
